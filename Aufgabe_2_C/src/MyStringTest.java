875c1885cce758e7dee6e0e18cff44731e9054f1
ph851res <philippressel@googlemail.com>
2021-01-07 12:40:50 +0100

    Aufgabe 1 added

--- Aufgabe_2_C/src/MyStringTest.java
+++ Aufgabe_2_C/src/MyStringTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 public class MyStringTest {
     private MyString myString;
     private Object anObject;
-    private Boolean result;
 
 
     @Test
@@ -12,27 +11,25 @@ public class MyStringTest {
         this.myString = new MyString("testStringSameObject");
         this.anObject = myString;
 
-        this.result = anObject.equals(myString);
+        boolean result = anObject.equals(myString);
 
-        assertTrue("Should be the same object, return true", result);
+        assertTrue(result);
 
         this.myString = null;
         this.anObject = null;
-        this.result = null;
     }
 
     @Test
     public void should_Be_Different_Type() {
         this.myString = new MyString("testStringWillDiffer");
-        this.anObject = new String ("testSrtingWillDiffer");
+        this.anObject = "testSrtingWillDiffer";
 
-        this.result = anObject.equals(myString);
+        boolean result = anObject.equals(myString);
 
-        assertFalse("Should not be the same Type, return false", result);
+        assertFalse(result);
 
         this.myString = null;
         this.anObject = null;
-        this.result = null;
     }
 
     @Test
@@ -40,13 +37,12 @@ public class MyStringTest {
         this.myString = new MyString("testStringSameType");
         this.anObject = new MyString("testStringSameType");
 
-        this.result = anObject.equals(myString);
+        boolean result = anObject.equals(myString);
 
-        assertTrue("Should be the same Type, return true", result);
+        assertTrue(result);
 
         this.myString = null;
         this.anObject = null;
-        this.result = null;
     }
 
     @Test
@@ -54,13 +50,12 @@ public class MyStringTest {
         this.myString = new MyString("testStringSameString");
         this.anObject = new MyString("testStringSameString");
 
-        this.result = anObject.equals(myString);
+        boolean result = anObject.equals(myString);
 
-        assertTrue("Should be the same String (and same Type), return true", result);
+        assertTrue(result);
 
         this.myString = null;
         this.anObject = null;
-        this.result = null;
     }
 
     @Test
@@ -68,12 +63,24 @@ public class MyStringTest {
         this.myString = new MyString("testStringA");
         this.anObject = new MyString("testStringB");
 
-        this.result = anObject.equals(myString);
+        boolean result = anObject.equals(myString);
 
-        assertFalse("The two strings should be different (but same Type), return false", result);
+        assertFalse(result);
 
         this.myString = null;
         this.anObject = null;
-        this.result = null;
+    }
+
+    @Test
+    public void should_Be_Different_Length() {
+        this.myString = new MyString("testStringA");
+        this.anObject = new MyString("testStringAB");
+
+        boolean result = anObject.equals(myString);
+
+        assertFalse(result);
+
+        this.myString = null;
+        this.anObject = null;
     }
 }
